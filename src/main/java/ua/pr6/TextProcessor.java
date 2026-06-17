0bda6f269b21ed26730e49400b2c3ccbf5c31911
Alina Borozniak <borozniakalina@gmail.com>
2026-06-17 09:54:36 +0300

    update code from checkstyle errors

--- src/main/java/ua/pr6/TextProcessor.java
+++ src/main/java/ua/pr6/TextProcessor.java
@@ -1,15 +1,23 @@
 package ua.pr6;
 
+/**
+ * Class for processing text: normalization, capitalization, and reversing words
+ */
 public class TextProcessor {
 
-    public String normalize(String text) {
+    /**
+     * Normalizes text by removing extra symbols and extra spaces
+     * @param text input text
+     * @return normalized text
+     */
+    public String normalize(final String text) {
         String lowered = text.toLowerCase();
         String[] words = lowered.split("\\s+");
         StringBuilder normalized = new StringBuilder();
 
         boolean firstWord = true;
 
-        for(String word : words) {
+        for (String word : words) {
             word = word.replaceAll("[^a-zA-Z']", "");
             word = word.replaceAll("^'+", "");
             word = word.replaceAll("'+$", "");
@@ -25,22 +33,34 @@ public class TextProcessor {
         return normalized.toString();
     }
 
-    public String capitalize(String text) {
+    /**
+     * Capitalizes the first letter of each word
+     * @param text input text
+     * @return text with capitalized words
+     */
+    public String capitalize(final String text) {
         String[] words = text.split("\\s+");
         StringBuilder capitalized = new StringBuilder();
 
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
-            if (word.isEmpty()) continue;
+            if (word.isEmpty()) {
+                continue;
+            }
             capitalized.append(word.substring(0, 1).toUpperCase()).append(word.substring(1));
-            if(i != words.length - 1) {
+            if (i != words.length - 1) {
                 capitalized.append(" ");
             }
         }
         return capitalized.toString();
     }
 
-    public String reverseOrder(String text) {
+    /**
+     * Reverses the order of words in the text
+     * @param text input text
+     * @return text with reversed word order
+     */
+    public String reverseOrder(final String text) {
         String[] words = text.trim().split("\\s+");
         StringBuilder reversed = new StringBuilder();
 
